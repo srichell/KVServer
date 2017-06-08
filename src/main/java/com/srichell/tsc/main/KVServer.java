b6c5ce517e676988515a16e0a775973ea83ebba2
sridhar <sridhar.chellappa@go-jek.com>
2017-06-08 14:13:31 +0530

    Created Dummy server class and its corresponding test class

---
+++ src/main/java/com/srichell/tsc/main/KVServer.java
@@ -0,0 +1,11 @@
+package com.srichell.tsc.main;
+
+/**
+ * Created by sridhar on 6/8/17.
+ */
+public class KVServer {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ test/main/java/com/srichell/tsc/main/KVServerTest.java
@@ -0,0 +1,10 @@
+package com.srichell.tsc.main;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by sridhar on 6/8/17.
+ */
+public class KVServerTest {
+
+}
